a3808064e8831c54e4c5ac2df991d2d7a7c79198
AMAN SINGLA <amansingla222@gmail.com>
2021-04-24 19:11:38 +0530

    Implemented Recommended Videos

--- src/components/RecommendedVideos.jsx
+++ src/components/RecommendedVideos.jsx
@@ -1,10 +1,69 @@
 import React from 'react'
 import './RecommendedVideos.css';
+import Video from './Video';
 
 function RecommendedVideos() {
     return (
         <div className='recommendedVideos'>
-            <h2>Here Recommended Videos will arrive which will be build later.</h2>
+            <h2>Recommended</h2>
+            <div className="recommendedVideos-videos">
+
+                <Video title='How to Become a Full Stack Web Developer' views='409K views' timestamp='6 months ago' channelImage='https://yt3.ggpht.com/ytc/AAUvwnjeHnMHZ7T341QLHqPhQPfsG6ugvg_SV6z6FkYqGw=s68-c-k-c0x00ffffff-no-rj' channel='CodeWithHarry' 
+                image='https://i.ytimg.com/vi/nknwAOtmtDk/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA_Fa0KUziH-xUnq-kyKwm0fvY3Ow'/>
+
+                <Video title='How to Become a Web Developer In 2020' views='161K views' timestamp='1 year ago' channelImage='https://yt3.ggpht.com/ytc/AAUvwnhIz_0Su6HhW6Ym50QCroJCAnF10X9xnnMDboN2=s68-c-k-c0x00ffffff-no-rj' channel='Web Dev Simplified' 
+                image='https://i.ytimg.com/vi/v0Bkxc3YeIc/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCrxUWiDy70yY23qr4D7fyDQVnSOQ'/>
+
+                <Video title='4 Steps to Become a Front End Developer in 2021' views='180K views' timestamp='7 months ago' channelImage='https://yt3.ggpht.com/ytc/AAUvwniwccxGvXvGzzwka5f73aPbmdxvEX4G_cUd7TEzkw=s68-c-k-c0x00ffffff-no-rj' channel='Clever Programmer' 
+                image='https://i.ytimg.com/vi/F3ztn4H6Ftg/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAmDrhOep4y6e_nh34coYk9mULaog'/>
+
+                <Video title='Complete Roadmap for Web Development || Noob to Advanced || Web Developer 2020' views='317K views' timestamp='7 months ago' channelImage='https://yt3.ggpht.com/ytc/AAUvwniuiil3MsNvPltVwGbgyVArM0iMBtfTe00CcHNN=s68-c-k-c0x00ffffff-no-rj' channel='Love Babbar' 
+                image='https://i.ytimg.com/vi/GLk7-imcjiI/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAS7hJwhlwF268sjI1dPyAeuKDDGA'/>
+
+                <Video title='7 Steps to Become a Web Developer || How to Become Web Developer in 2020 || Web Developer Guideline' views='61K views' timestamp='10 months ago' channelImage='https://yt3.ggpht.com/ytc/AAUvwnixvIQZRJsC-980VBOJCV2PoJl1KB5rV2zDr4zm=s68-c-k-c0x00ffffff-no-rj' channel='Programming Hero' 
+                image='https://i.ytimg.com/vi/kUMV7jmbpPY/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAQ4OL4eVSDG7faYWGBNpVWbrDOgA'/>
+
+                <Video title='How To Become a Full Stack Developer In 2020' 
+                views='325K views'
+                timestamp='4 months ago'
+                channelImage='https://yt3.ggpht.com/ytc/AAUvwngAFkYK5AVn9PZLSUj4hJ5RH3MVlQ-XyLfY9shx=s68-c-k-c0x00ffffff-no-rj'
+                channel='Tiff In Tech'
+                image='https://i.ytimg.com/vi/uD27CZEiuGk/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAbPv36e1pAiBjH6WWnuR3q-3agRg'
+                />
+
+                <Video title='Backend Developer Guide 2021'
+                views='58K views'
+                timestamp='3 months ago'
+                channelImage='https://yt3.ggpht.com/ytc/AAUvwniwccxGvXvGzzwka5f73aPbmdxvEX4G_cUd7TEzkw=s68-c-k-c0x00ffffff-no-rj'
+                channel='Clever Programmer'
+                image='https://i.ytimg.com/vi/-bjJetWnNZg/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC6QCfWOJv1spFt1IdM04kqBJu8iQ'
+                />
+
+                <Video title='JavaScript Tutorial for Beginners: Learn JavaScript in 1 Hour'
+                views='4.5M views'
+                timestamp='3 years ago'
+                channelImage='https://yt3.ggpht.com/ytc/AAUvwnj82Lirw0dg6V5pJWAcWdG22OESyldUcDwAFEqQWg=s68-c-k-c0x00ffffff-no-rj'
+                channel='Programming With Mosh'
+                image='https://i.ytimg.com/vi/W6NZfCO5SIk/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDOBWmb1XE5qRd1xinBxZPfYfXoEg'
+                />
+
+                <Video title='Learn JavaScript - Full Course for Beginners'
+                views='5.9M views'
+                timestamp='2 years ago'
+                channelImage='https://yt3.ggpht.com/ytc/AAUvwnifaQZvAunS0OFb2y_cieoVjLCVjqQW8Exf3BC1gg=s68-c-k-c0x00ffffff-no-rj'
+                channel='freeCodeCamp.org'
+                image='https://i.ytimg.com/vi/PkZNo7MFNFg/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCWGmAFXkqN3F-QqhVaqBWJY2ltFw'
+                />
+
+                <Video title='10 JavaScript Projects in 10 Hours - Coding Challenge 🔥'
+                views='721K views'
+                timestamp='7 months ago'
+                channelImage='https://yt3.ggpht.com/ytc/AAUvwni7l--cxhsMtvLBc7eqUNgsFIW6rsAE7fbSt4fz=s68-c-k-c0x00ffffff-no-rj'
+                channel='Florin Pop'
+                image='https://i.ytimg.com/vi/dtKciwk_si4/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDopEVsTMNPEWl8FacIr7df4LImdw'
+                />
+
+            </div>
         </div>
     )
 }
